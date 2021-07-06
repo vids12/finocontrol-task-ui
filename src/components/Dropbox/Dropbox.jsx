9e1f862d23d12eeb8bb99bf36714173add6db8ea
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-07 01:14:10 +0530

    feat: dropdown

---
+++ src/components/Dropbox/Dropbox.jsx
@@ -0,0 +1,10 @@
+import "./Dropbox.css";
+import { Link } from "react-router-dom";
+
+export function Dropbox() {
+    return (
+        <div className="dropbox">
+            <Link to="/finshiksha">FinShiksha</Link>
+        </div>
+    );
+}
