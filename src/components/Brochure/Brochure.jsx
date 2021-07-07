950426e37ad4f764e2860e9b1457e3b79adea265
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-07 19:47:17 +0530

    add: brochure

---
+++ src/components/Brochure/Brochure.jsx
@@ -0,0 +1,9 @@
+import "./Brochure.css";
+
+export function Brochure() {
+    return (
+        <button className="brochure">
+            <i class="fa fa-download" aria-hidden="true"></i>Download Course Brochure
+        </button>
+    );
+} 
