0116299a1e373faaffc694baedba710c92109cb4
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-08 01:25:40 +0530

    add: content

---
+++ src/components/Content/contentData.js
@@ -0,0 +1,22 @@
+export const contentData = [
+    {
+        title: 'Applied Financial Statement Analysis Certification (Mandatory)',
+        content: 'Check the Course Details'
+    },
+    {
+        title: 'Equity Valuation Certification (Mandatory)',
+        content: 'Check the Course Details'
+    },
+    {
+        title: 'Investment Banking Certification (Mandatory)',
+        content: 'Check the Course Details'
+    },
+    {
+        title: 'Credit Analysis Certification (Mandatory)',
+        content: 'Check the Course Details'
+    },
+    {
+        title: 'Applied Derivatives Certification (Mandatory)',
+        content: 'Check the Course Details'
+    },
+];
