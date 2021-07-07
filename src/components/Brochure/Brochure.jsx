60f972afb68827a0df7b844098822b8b500a74fe
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-07 20:10:45 +0530

    change: class to className

--- src/components/Brochure/Brochure.jsx
+++ src/components/Brochure/Brochure.jsx
@@ -3,7 +3,7 @@ import "./Brochure.css";
 export function Brochure() {
     return (
         <button className="brochure">
-            <i class="fa fa-download" aria-hidden="true"></i>Download Course Brochure
+            <i className="fa fa-download" aria-hidden="true"></i>Download Course Brochure
         </button>
     );
 } 
