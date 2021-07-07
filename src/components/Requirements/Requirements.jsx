b2fea796b927c3d495e7bf61e14751814e42a731
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-07 21:37:26 +0530

    add: requirements

---
+++ src/components/Requirements/Requirements.jsx
@@ -0,0 +1,15 @@
+import "./Requirements.css";
+
+export function Requirements() {
+    return (
+        <div className="requirements">
+            <h1 className="landing-page-title">
+                Requirements
+                <div className="overview-box"></div>
+            </h1>
+            <ul>
+                <li>Program ensures building of knowledge from Basics. No specific requirements needed from the participants end.</li>
+            </ul>
+        </div>
+    );
+}
