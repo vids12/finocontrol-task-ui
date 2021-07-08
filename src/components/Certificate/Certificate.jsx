a76f7558fc4260c97c48a4c2ba5334f765dd890e
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-08 23:48:41 +0530

    feat: responsive certificate

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -3,15 +3,15 @@ import "./Certificate.css";
 
 export  function Certificate() {
     return (
-        <div className="certificate">
+        <div className="certificate-main-div">
             <div>
                 <CertificateDesign />
             </div>
+        <div className="certificate">
             <h1 className="landing-page-title">
                 Certification
-                <div className="overview-box"></div>
             </h1>
-            <section>
+            <section className="certificate-content">
                 <p>
                     Each course will have an online assessment at the end of each Course. The final Certification will be an open Online test, which would be untimed, and open book. We would try and keep the assessment as close to a real life situation as possible, where you will have all the tools that you would have in real life at your disposal.
                 </p>
@@ -22,6 +22,7 @@ export  function Certificate() {
                     On successful completion of the Analyst Program, the student is awarded a Certificate of Completion for the entire Program.
                 </p>
             </section>
+        </div>
         </div>
     )
 }
