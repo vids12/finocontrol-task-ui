d56c90378ff7f502913c488f0f9a8553fe6f9157
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-09 00:38:24 +0530

    add: images

--- src/components/Course3/Course3.jsx
+++ src/components/Course3/Course3.jsx
@@ -3,7 +3,7 @@ import "../Course1/Course1.css";
 export function Course3() {
     return (
         <div className="course1">
-            <div>
+            <div className="parent">
                 <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" className="course-image"alt="o" />
                 <div className="inner-border"></div>
             </div>
