0f0ed0df80ebe7193b12749721373c2300a7e8a2
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-09 16:39:22 +0530

    update: blog page

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -7,8 +7,21 @@ export function Blogs() {
             <Navbar />
             <div className="blogs">
                 <h1 className="title">Blogs</h1>
-                <p>Coming Soon!!</p>
-            </div>
+                <section className="blog-section">
+                    <div className="ow section">
+                        <h3>Heading</h3>
+                        <div className="container "><p>Blog sub-heading</p>
+                            <a target="_blank" className="link" href="##">View the blog</a> </div>
+                    </div>
+                </section>
+                <section className="blog-section">
+                    <div className="section">
+                        <h3>Heading</h3>
+                        <div className="container "><p>Blog sub-heading</p>
+                            <a target="_blank" className="link" href="##">View the blog</a> </div>
+                    </div>
+                </section>
+            </div> 
         </div>
     )
 }
