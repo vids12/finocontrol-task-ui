e1db5e63afcdc9fd0c3a09a0d5c89d6096aa1d52
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-09 16:57:57 +0530

    add: landing page image

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -6,6 +6,7 @@ export function LandingPage() {
         <div className="landing-page">
             <Navbar />
             <div className="landing-image">
+                {/* <img src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" alt="f" className="landing-image" /> */}
                 <BannerOnImg />
             </div>
             <Banner />
